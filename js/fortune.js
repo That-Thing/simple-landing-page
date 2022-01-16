560465ffd1b93af4553fef7eeb51fe9c12b15d76
Sen <41160238+That-Thing@users.noreply.github.com>
2022-01-16 12:38:37 -0500

    Update fortune.js
    Updated api url

--- js/fortune.js
+++ js/fortune.js
@@ -1,4 +1,4 @@
-fetch('https://api.neko.airforce/api/fortune').then(response => response.json())
+fetch('https://api.sen.cat/api/fortune').then(response => response.json())
 	.then((data) => {
 	    fortune = data["fortune"];
         document.getElementById("message").innerHTML = fortune;
